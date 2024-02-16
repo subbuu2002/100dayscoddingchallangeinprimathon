bb6e65248d31e0ec7e25fe15cf1239864555a736
subhash <subhashpatidar2002@gmail.com>
2024-02-16 19:14:30 +0530

    file saving

---
+++ dsaDay1.js
@@ -0,0 +1,10 @@
+// Program in JavaScript to check if the number is even or odd
+const num = prompt("Enter a number: ");
+
+//check if the number is even
+if(num % 2 == 0) 
+    console.log("The number is Even.");
+
+// if the number is odd
+else 
+    console.log("The number is odd.");
